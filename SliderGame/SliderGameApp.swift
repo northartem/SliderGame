48fac9ca5b754452b8d9050bbbec4bb1f313013a
AssanCred <temix@live.ru>
2023-06-19 20:25:19 +0700

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Артём Латушкин on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
